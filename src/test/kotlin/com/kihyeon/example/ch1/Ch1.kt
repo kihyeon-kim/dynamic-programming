b39d55ebc9cacda2a52f451944f04ae11276e189
kihyeon-kim <enki.heart.kim@gmail.com>
2020-03-27 23:12:22 +0900

    챕터 1. 1-7, 1-8, 1-10

--- src/test/kotlin/com/kihyeon/example/ch1/Ch1.kt
+++ src/test/kotlin/com/kihyeon/example/ch1/Ch1.kt
@@ -131,4 +131,108 @@ internal class Ch1 {
 
         println(towerOfHanoi('s', 'd', 'e', 3))
     }
+
+    @Test
+    fun `1-7`() {
+        class Node(
+            val next: Node?,
+            val data: Int
+        )
+
+        fun traverse1(head: Node?) {
+            if (head != null) {
+                traverse1(head.next)
+                println("${head.data}")
+            }
+        }
+
+        fun traverse2(head: Node?) {
+            if (head != null) {
+                println("${head.data}")
+                traverse2(head.next)
+            }
+        }
+
+        val testList = Node(
+            Node(
+                Node(
+                    Node(
+                        null,
+                        4
+                    ),
+                    3
+                ),
+                2
+            ),
+            1
+        )
+
+        println("# traverse1")
+        println(traverse1(testList))
+        println("# traverse2")
+        println(traverse2(testList))
+    }
+
+    @Test // 중위 순회 탐색 함수
+    fun `1-8`() {
+        class Node(
+            val left: Node?,
+            val data: Int,
+            val right: Node?
+        )
+
+        // 이 코드는 선행 재귀 또는 후행 재귀 어느쪽에도 해당되지 않는다
+        fun inOrder(ptr: Node?) {
+            if (ptr == null) {
+                return
+            }
+
+            inOrder(ptr.left)
+            println("data: ${ptr.data}")
+            inOrder(ptr.right)
+        }
+
+        fun inOrder2(ptr: Node?) {
+            if (ptr == null) {
+                return
+            }
+
+            if (ptr.left != null) {
+                inOrder2(ptr.left)
+            }
+
+            println("data: ${ptr.data}")
+
+            if (ptr.right != null) {
+                inOrder2(ptr.right)
+            }
+        }
+    }
+
+    @Test
+    fun `1-10`() {
+
+        // void swap(int *a, int *b) {
+        //     *a ^= *b;
+        //     *b ^= *a;
+        //     *a ^= *b;
+        // }
+
+        fun bubbleSort(arr: Array<Int?>, n: Int) {
+            for (i in 0 until n - 1) { // 0 .. 6
+                for (j in 0 until n - i - 1) { // 0 .. 5
+                    if (arr[j]!! > arr[j + 1]!!) {
+                        // swap
+                        val temp = arr[j]
+                        arr[j] = arr[j + 1]
+                        arr[j + 1] = temp
+                    }
+                }
+            }
+        }
+
+        val arr: Array<Int?> = arrayOf(9, 6, 2, 12, 11, 9, 3, 7)
+        bubbleSort(arr, 8)
+        println(arr.joinToString())
+    }
 }
