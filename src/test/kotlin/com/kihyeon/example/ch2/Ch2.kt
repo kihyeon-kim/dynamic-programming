802af7055164eb18a038cb09fc1af8f31358584d
kihyeon-kim <enki.heart.kim@gmail.com>
2020-03-31 21:09:53 +0900

    챕터 2. 2-4, 2-5

--- src/test/kotlin/com/kihyeon/example/ch2/Ch2.kt
+++ src/test/kotlin/com/kihyeon/example/ch2/Ch2.kt
@@ -73,4 +73,74 @@ internal class Ch2 {
 
         println(minCost(0, 3))
     }
+
+    @Test
+    fun `2-4`() {
+        val size = 10
+        val memo = arrayOfNulls<Int>(size + 1)
+
+        fun fibonacci(n: Int): Int {
+            if (memo[n] != null) {
+                return memo[n]!!
+            }
+
+            if (n == 1 || n == 2) {
+                memo[n] = 1
+            } else {
+                memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            }
+            return memo[n]!!
+        }
+
+        println(fibonacci(size))
+    }
+
+    @Test
+    fun `2-5`() {
+        val cost: Array<IntArray> = arrayOf(
+            intArrayOf(0, 10, 75, 94),
+            intArrayOf(-1, 0, 35, 50),
+            intArrayOf(-1, -1, 0, 80),
+            intArrayOf(-1, -1, -1, 0)
+        )
+
+        val memo = arrayOf(
+            arrayOfNulls(4),
+            arrayOfNulls(4),
+            arrayOfNulls(4),
+            arrayOfNulls<Int>(4)
+        )
+
+        fun minCost(s: Int, d: Int): Int {
+            if (s == d || s == d - 1) {
+                return cost[s][d]
+            }
+
+            // 값이 계산되지 않은 경우
+            if (memo[s][d] == null) {
+                var minValue = cost[s][d]
+
+                (s + 1 until d)
+                    .forEach {
+                        val temp = minCost(s, it) + minCost(it, d)
+                        if (temp < minValue) {
+                            minValue = temp
+                        }
+                    }
+
+                memo[s][d] = minValue
+            }
+
+            return memo[s][d]!!
+        }
+
+        println(minCost(0, 3))
+
+        for (i in memo.indices) {
+            for (j in memo[i].indices) {
+                print(" ${memo[i][j]}")
+            }
+            println()
+        }
+    }
 }
